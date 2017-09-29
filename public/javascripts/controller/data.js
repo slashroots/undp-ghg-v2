d980092d2d47a8451a5b7e68441aba27dea524aa
Matthew Budram <matjames007@gmail.com>
2017-09-28 20:32:58 -0500

    I have enabled inline edits that are persisted on the database immediately.  The lookups are loaded on runtime and a script looks for changes to cell edits and replaces the value with the actual object for the db writes.  Still a WIP.

--- public/javascripts/controller/data.js
+++ public/javascripts/controller/data.js
@@ -9,6 +9,9 @@ angular.module('undp-ghg-v2')
     function($scope, $q, $location, $routeParams, UserFactory, SectorFactory, CategoryFactory, GasFactory,
       AdminUserFactory, InventoryFactory, ActivityFactory, UnitFactory, DataFactory) {
 
+      var editable = false;
+      $scope.dataGridOptions = {};
+
       /*
         Setup the tabs for viewing
       */
@@ -25,6 +28,14 @@ angular.module('undp-ghg-v2')
       };
 
       $scope.filtered = function() {
+        InventoryFactory.get({
+          id: $scope.selectedInventory
+        }, function(item) {
+          if (item.in_status == 'opened') {
+            editable = true;
+          }
+        });
+
         //lookup based on filter applied
         DataFactory.query({
             in_inventory: $scope.selectedInventory
@@ -32,6 +43,7 @@ angular.module('undp-ghg-v2')
             if (dataValues) {
               $scope.isAvailable = true;
             }
+            $scope.dataGridOptions = getGrid(editable);
             $scope.dataValues = dataValues;
           },
           function(error) {
@@ -39,106 +51,164 @@ angular.module('undp-ghg-v2')
           });
       }
 
-      // DataFactory.create({
-      //     da_variable_type: "EF",
-      //     da_data_value: 10,
-      //     da_notes: "TESTING NOTES",
-      //     da_uncertainty_min: -5,
-      //     da_uncertainty_max: 5
-      //   }, function(success) {
-      //     console.log(success);
-      //   }, function(error) {
-      //     console.log(error);
-      //   }
-      // );
-
+      /**
+       * Data Factories
+       **/
+      $scope.inventories = InventoryFactory.query();
       $scope.categories = CategoryFactory.query();
+      $scope.units = UnitFactory.query();
+      $scope.gases = GasFactory.query();
+      $scope.activities = ActivityFactory.query();
 
-
-      //setup the tables
-      $scope.dataGridOptions = {
-        data: 'dataValues',
-        columnDefs: [{
-            field: 'da_variable_type',
-            displayName: 'Variable Type',
-            enableCellEdit: true,
-            width: 200,
-            editDropdownValueLabel: 'variableType',
-            editableCellTemplate: 'ui-grid/dropdownEditor',
-            editDropdownOptionsArray: [
-              {variableType: 'EF'},
-              {variableType: 'AD'}
-            ]
-          },
-          {
-            field: 'ca_category',
-            displayName: 'Category Name',
-            enableCellEdit: true,
-            width: 200,
-            editableCellTemplate: 'ui-grid/dropdownEditor',
-            editDropdownValueLabel: 'ca_code_name',
-            editDropdownOptionsArray: $scope.categories,
-            editDropdownIdLabel: '_id'
-          },
-          {
-            field: 'ac_name',
-            displayName: 'Activity',
-            enableCellEdit: true,
-            width: 200
-          },
-          {
-            field: 'da_data_value',
-            displayName: 'Activity Amount/Value',
-            enableCellEdit: true,
-            type: 'number',
-            width: 200
-          },
-          {
-            field: 'un_unit_symbol',
-            displayName: 'Unit',
-            enableCellEdit: true,
-            width: 200
-          },
-          {
-            field: 'da_date',
-            displayName: 'Year',
-            enableCellEdit: true,
-            cellFilter: "date: 'yyyy'",
-            width: 200,
-            type: 'date'
-          },
-          {
-            field: 'ga_gas_name',
-            displayName: 'Assoc. Gas',
-            enableCellEdit: true,
-            width: 200
-          },
-          {
-            field: 'da_uncertainty_min',
-            displayName: 'Uncertainty (min)',
-            enableCellEdit: true,
-            width: 200
-          },
-          {
-            field: 'da_uncertainty_max',
-            displayName: 'Uncertainty (max)',
-            enableCellEdit: true,
-            width: 200
-          },
-          {
-            field: 'nk_notation_key',
-            displayName: 'NK',
-            enableCellEdit: true,
-            width: 200
-          }
-        ]
+      getGrid = function(editable) {
+        //setup the tables
+        return {
+          data: 'dataValues',
+          enableFiltering: true,
+          enableGridMenu: true,
+          enableCellEditOnFocus: editable,
+          columnDefs: [{
+              field: 'da_variable_type',
+              displayName: 'Variable Type',
+              enableCellEdit: editable,
+              width: 200,
+              editDropdownValueLabel: 'variableType',
+              editDropdownIdLabel: 'variableType',
+              editableCellTemplate: 'ui-grid/dropdownEditor',
+              editDropdownOptionsArray: [{
+                  variableType: 'EF'
+                },
+                {
+                  variableType: 'AD'
+                }
+              ]
+            },
+            {
+              field: 'ca_category.ca_code_name',
+              displayName: 'Category Name',
+              enableCellEdit: editable,
+              cellTooltip: function(row, col) {
+                if (row.entity.ca_category) {
+                  return 'Code: ' + row.entity.ca_category.ca_code + ' Name: ' + row.entity.ca_category.ca_code_name;
+                } else {
+                  return '';
+                }
+              },
+              width: 200,
+              editableCellTemplate: 'ui-grid/dropdownEditor',
+              editDropdownValueLabel: 'ca_code_name',
+              editDropdownOptionsArray: $scope.categories,
+              editDropdownIdLabel: '_id'
+            },
+            {
+              field: 'ac_activity.ac_name',
+              displayName: 'Activity',
+              enableCellEdit: editable,
+              width: 200,
+              editableCellTemplate: 'ui-grid/dropdownEditor',
+              editDropdownValueLabel: 'ac_name',
+              editDropdownOptionsArray: $scope.activities,
+              editDropdownIdLabel: '_id'
+            },
+            {
+              field: 'da_data_value',
+              displayName: 'Activity Amount/Value',
+              enableCellEdit: editable,
+              type: 'number',
+              width: 200
+            },
+            {
+              field: 'un_unit.un_unit_symbol',
+              displayName: 'Unit',
+              enableCellEdit: editable,
+              width: 200,
+              editableCellTemplate: 'ui-grid/dropdownEditor',
+              editDropdownValueLabel: 'un_unit_symbol',
+              editDropdownOptionsArray: $scope.units,
+              editDropdownIdLabel: '_id'
+            },
+            {
+              field: 'da_date',
+              displayName: 'Year',
+              enableCellEdit: editable,
+              cellFilter: "date: 'yyyy'",
+              width: 200,
+              type: 'date'
+            },
+            {
+              field: 'ga_gas.ga_gas_name',
+              displayName: 'Assoc. Gas',
+              enableCellEdit: editable,
+              width: 200,
+              editableCellTemplate: 'ui-grid/dropdownEditor',
+              editDropdownValueLabel: 'ga_gas_name',
+              editDropdownOptionsArray: $scope.gases,
+              editDropdownIdLabel: '_id'
+            },
+            {
+              field: 'da_uncertainty_min',
+              displayName: 'Uncertainty (min)',
+              enableCellEdit: editable,
+              width: 200
+            },
+            {
+              field: 'da_uncertainty_max',
+              displayName: 'Uncertainty (max)',
+              enableCellEdit: editable,
+              width: 200
+            },
+            {
+              field: 'nk_notation_key',
+              displayName: 'NK',
+              enableCellEdit: editable,
+              width: 200
+            }
+          ]
+        };
       };
 
+      $scope.lookupEditor = function(rowEntity, columnDef, newValue, oldValue) {
+        if (newValue != oldValue) {
+          if (columnDef.field == 'ca_category.ca_code_name') {
+            CategoryFactory.get({
+                id: rowEntity.ca_category.ca_code_name
+              },
+              function(item) {
+                rowEntity.ca_category = item;
+              });
+          } else if (columnDef.field == 'ac_activity.ac_name') {
+            ActivityFactory.get({
+                id: rowEntity.ac_activity.ac_name
+              },
+              function(item) {
+                rowEntity.ac_activity = item;
+              });
+          } else if (columnDef.field == 'un_unit.un_unit_symbol') {
+            UnitFactory.get({
+                id: rowEntity.un_unit.un_unit_symbol
+              },
+              function(item) {
+                rowEntity.un_unit = item;
+              });
+          } else if (columnDef.field == 'ga_gas.ga_gas_name') {
+            GasFactory.get({
+                id: rowEntity.ga_gas.ga_gas_name
+              },
+              function(item) {
+                rowEntity.ga_gas = item;
+              });
+          }
+        }
+      };
+
+      //This is run after a ROW edited event
       $scope.saveRow = function(rowEntity) {
         //set a promise
         // var promise = $q.defer();
 
-        var dataRecord = DataFactory.edit({id: rowEntity._id}, rowEntity);
+        var dataRecord = DataFactory.edit({
+          id: rowEntity._id
+        }, rowEntity);
         $scope.gridApi.rowEdit.setSavePromise(rowEntity, dataRecord.$promise);
 
       };
@@ -147,17 +217,8 @@ angular.module('undp-ghg-v2')
         //set gridApi on scope
         $scope.gridApi = gridApi;
         gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
+        gridApi.edit.on.afterCellEdit($scope, $scope.lookupEditor);
       };
 
-
-
-
-      /**
-       * Data Factories
-       **/
-
-      InventoryFactory.query(function(inventories) {
-        $scope.inventories = inventories;
-      });
     }
   ]);
